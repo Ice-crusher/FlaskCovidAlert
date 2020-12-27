347d9210020a5268bb9c4404b080a5ac4e439bb7
Ice <andryxa121314@gmail.com>
2020-12-27 01:44:17 +0100

    ss

--- extensions.py
+++ extensions.py
@@ -8,7 +8,3 @@ ma = Marshmallow()
 fcmApiKey = os.environ.get('FCM_API_KEY')
 # fcmApiKey = "AAAAujOMeAE:APA91bEVG034XkqY-UGjen3iLkkj1XUEkb7yEfudgKj5qyabvdD-gUJkeAg3tPPuXPAZRgGAuDY2eDTbfZ5NPqF__OchVvfnlBTvxz5EwM1XLifLxm528pygZCRCSRT2RQs5C3hOXJgQ"
 mainWebSiteUrl = "https://www.gov.pl/web/koronawirus/wiadomosci"
-
-def model_exists(model_class):
-    engine = db.get_engine(bind=model_class.__bind_key__)
-    return model_class.metadata.tables[model_class.__tablename__].exists(engine)
